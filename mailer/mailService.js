e8f85c56d7a84666fc47031b7f0455e963a6d4d0
VoAnhPhi1207 <voanhphi.dev@gmail.com>
2025-05-15 14:31:24 +0700

    Fix | voanhphi | Fix Layout Template Email

--- mailer/mailService.js
+++ mailer/mailService.js
@@ -4,6 +4,9 @@ const path = require('path');
 const handlebars = require('handlebars');
 require('dotenv').config();
 
+console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
